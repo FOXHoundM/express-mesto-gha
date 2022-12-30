ebae739ea1b623adf4422b63173f6360c06921c0
Mikael Gulmamedov <4216623@gmail.com>
2022-12-30 20:50:58 +0300

    celebrate signup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
+const isURL = require('validator/lib/isURL');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
@@ -16,7 +18,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom((value) => {
+      if (!isURL(value)) {
+        throw new Error('Ошибка валидации. Введён не URL');
+      }
+      return value;
+    }),
+  }),
+}), createUser);
 
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
